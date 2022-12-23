136f79c1a33286b8aeaed259bfeb495f6b65edde
Dmytro Voronov <dimolg22@gmail.com>
2022-12-23 15:26:33 +0200

    add geography page

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -2,6 +2,7 @@ import Product from "../models/Product.js";
 import ProductState from "../models/ProductState.js";
 import User from "../models/User.js";
 import Transaction from "../models/Transaction.js";
+import getCountryIso3 from 'country-iso-2-to-3'
 
 
 export const getProducts = async (req, res) => {
@@ -57,4 +58,25 @@ export const getTransaction = async (req, res) => {
      }catch (e) {
          res.status(404).json({ message: e.message });
      }
+}
+export const getGeography = async (req, res) => {
+    try {
+        const users = await User.find()
+        const mappedLocation = users.reduce((acc, {country})=>{
+            const countryISO3 = getCountryIso3(country)
+            if (!acc[countryISO3]){
+                acc[countryISO3] = 0
+            }
+            acc[countryISO3]++
+            return acc
+        }, {})
+        const formattedLocation = Object.entries(mappedLocation).map(
+            ([country, count]) => {
+                return {id: country, value: count}
+            }
+        )
+        res.status(200).json(formattedLocation)
+    }catch (e) {
+        res.status(404).json({ message: e.message });
+    }
 }
--- server/routes/client.js
+++ server/routes/client.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import {getCustomers, getProducts, getTransaction} from "../controllers/client.js";
+import {getCustomers, getGeography, getProducts, getTransaction} from "../controllers/client.js";
 
 const router = Router()
 
 router.get('/products', getProducts)
 router.get('/customers', getCustomers)
 router.get('/transactions', getTransaction)
+router.get('/geography', getGeography)
 
 export default router
